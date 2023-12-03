8ed1fc679df809f8a8cc486bae05523ce2109abd
Nikola <nzencic@gmail.com>
2023-12-03 13:39:19 +0100

    fifth commit

--- index.js
+++ index.js
@@ -1,133 +1,82 @@
-function knightMoves(startPosition, endPosition, positions = [startPosition], savedPos = [], accuratePositions = []) {
-  // const moves = [[+2, -1], [+2, +1], [+1, +2], [-1, +2], [-1, +2], [-2, +1],
-  // [-2, -1], [-1, -2], [+1, -2]];
-  const checkPosition = (arr, x, y) => {
-    for (let j = 0; j < arr.length; j++) {
-      for (let i = 0; i < arr[j].length; i++) {
-        console.log(arr[j][i][0], arr[j][i][1]);
-        if (arr[j][i][0] === x && arr[j][i][1] === y) {
-          return false;
-        }
-        continue;
+function knightMoves(startPosition, endPosition) {
+  const createBoard = (arr = []) => {
+    const rows = 8;
+    const columns = 8;
+    for (let i = 0; i < rows; i++) {
+      arr[i] = [];
+      for (let j = 0; j < columns; j++) {
+        arr[i][j] = false;
       }
-      continue;
     }
-    return true;
+    return arr;
   };
-  const moves = {
-    moveX: +2,
-    moveY: -1,
-    next: {
-      moveX: +2,
-      moveY: +1,
-      next: {
-        moveX: +1,
-        moveY: +2,
-        next: {
-          moveX: -1,
-          moveY: +2,
-          next: {
-            moveX: -1,
-            moveY: +2,
-            next: {
-              moveX: -2,
-              moveY: +1,
-              next: {
-                moveX: -2,
-                moveY: -1,
-                next: {
-                  moveX: -1,
-                  moveY: -2,
-                  next: {
-                    moveX: +1,
-                    moveY: -2,
-                    next: null,
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-    },
+  const createBoard2 = (arr = []) => {
+    const rows = 8;
+    const columns = 8;
+    for (let i = 0; i < rows; i++) {
+      arr[i] = [];
+      for (let j = 0; j < columns; j++) {
+        arr[i][j] = -1;
+      }
+    }
+    return arr;
   };
-  if (startPosition[0] === endPosition[0] && startPosition[1] === endPosition[1]) {
-    return startPosition;
+  if (startPosition === null) {
+    return null;
   }
+  const dX = [+2, +2, +1, -1, -2, -2, -1, +1];
+  const dY = [-1, +1, +2, +2, +1, -1, -2, -2];
 
   const Q = [];
-
-  if (savedPos.length < 8 && accuratePositions.length === 0) { // change this
-    Q.unshift(startPosition[0], startPosition[1]);
-  } else if (Q.length < 1) {
-    for (let j = 0; j < savedPos.length; j++) {
-      for (let i = 0; i < savedPos[j].length; i++) {
-        if (savedPos[j].length > savedPos[1].length) {
-          j++;
-          const temp = savedPos[j];
-          const temp2 = savedPos[0];
-          savedPos[0] = temp;
-          savedPos.push(temp2);
-          savedPos.shift();
-          // savedPos[j] = savedPos[savedPos.length - 1];
-          // savedPos[savedPos.length - 1] = savedPos[0];
-          j--;
+  const nodes = [];
+  const board = createBoard();
+  const board2 = createBoard2();
+  Q.push(startPosition);
+  while (Q.length !== 0) {
+    const current = Q.shift();
+    for (let i = 0; i < 8; i++) {
+      const newX = current[0] + dX[i];
+      const newY = current[1] + dY[i];
+      if ((newX < 8 && newX > -1) && (newY < 8 && newY > -1)) {
+        if (board[newX][newY] === false) {
+          Q.push([newX, newY]);
+          board[newX][newY] = true;
+          if (endPosition[0] === newX && endPosition[1] === newY) {
+            break;
+          }
+          board2[newX][newY] = i;
         }
-        Q.unshift(savedPos[j][savedPos[i].length - 1][0], savedPos[j][savedPos[i].length - 1][1]);
-        // const temp = savedPos[j];
-        // savedPos[j] = savedPos[savedPos.length - 1];
-        // savedPos[savedPos.length - 1] = temp;
-        positions.pop();
-        positions.push([Q[0], Q[1]]);
+      }
+    }
+  }
+  Q.push(endPosition);
+  while (Q.length !== 0) {
+    const current = Q[Q.length - 1];
+
+    const newX = current[0];
+    const newY = current[1];
+    if (board2[newX][newY] === -1) {
+      board2[newX][newY] = 0;
+    }
+    if ((newX - dX[board2[newX][newY]] < 8 && newX - dX[board2[newX][newY]] > -1)
+    && (newY - dY[board2[newX][newY]] < 8 && newY - dY[board2[newX][newY]] > -1)) {
+      Q.push([newX - dX[board2[newX][newY]], newY - dY[board2[newX][newY]]]);
+      if (startPosition[0] === Q[Q.length - 1][0] && startPosition[1] === Q[Q.length - 1][1]) {
+        nodes.push(Q.reverse());
         break;
       }
-      break;
-    }
-  }
-  let move = moves;
-  while (move) {
-    let x = Q.at(-2);
-    let y = Q.at(-1);
-    if ((x + move.moveX < 7 && x + move.moveX > 0) && (y + move.moveY < 7 && y + move.moveY > 0)) {
-      // positions.push([x, y]);
-
-      x += move.moveX;
-      y += move.moveY;
-      if (checkPosition(savedPos, x, y) === true && checkPosition(accuratePositions, x, y) === true) {
-        Q.unshift(x, y);
-        Q.splice(2, 2);
-        positions.push([x, y]);
+    } else {
+      Q.push([newX + dX[board2[newX][newY]], newY + dY[board2[newX][newY]]]);
+      if (startPosition[0] === Q[Q.length - 1][0] && startPosition[1] === Q[Q.length - 1][1]) {
+        nodes.push(Q.reverse());
+        break;
       }
     }
-    if (positions[positions.length - 1][0] === endPosition[0] && positions[positions.length - 1][1] === endPosition[1]) {
-      break;
-    }
-    move = move.next;
   }
-  // positions.shift();
-  if (positions.length > 2 && (accuratePositions.length !== 0 || savedPos.length > 7)) {
-    positions.shift();
-    savedPos[0].push(positions[0]);
-    savedPos[0].push(positions[1]);
-    // let accuratePositions = savedPos.pop();
-  } else if (positions.length === 2 && accuratePositions.length !== 0 || savedPos.length > 7) {
-    positions.shift();
-    savedPos[0].push(positions[0]);
-  } else if (positions.length < 2 && accuratePositions.length !== 0 || savedPos.length > 7) {
-    positions.shift();
-    // savedPos[savedPos.length - 1].push(positions[0]);
-  }
-  if (savedPos.length < 8 && accuratePositions.length === 0) {
-    savedPos.push([positions[0], positions[1]]);
-  }
-  if (savedPos[savedPos.length - 1][savedPos[savedPos.length - 1].length - 1][0] === endPosition[0]
-    && savedPos[savedPos.length - 1][savedPos[savedPos.length - 1].length - 1][1] === endPosition[1]) {
-    accuratePositions.push(savedPos.pop());
-  }
-  // positions = [];
-  knightMoves(startPosition, endPosition, positions = [startPosition], savedPos, accuratePositions);
 
-  return { checkPosition };
+  console.log(`You made it in ${nodes[0].length - 1} moves! Here's your path:`);
+  nodes.forEach((node) => {
+    node.forEach((elem) => console.log(elem));
+  });
 }
-
-console.log(knightMoves([3, 3], [4, 3]));
+knightMoves([1, 1], [6, 6]);
